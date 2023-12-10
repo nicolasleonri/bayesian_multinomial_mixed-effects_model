e975657d784ad4fa419312c079e93c77784b9676
nicolasleonri <nicolas.leonri@gmail.com>
2023-12-10 13:46:04 +0100

    now with logreg

--- main.R
+++ main.R
@@ -7,6 +7,8 @@ library(remotes)
 # Package to deal with results (its not an official CRAN package yet. Needs package "remotes")
 #remotes::install_github('m-clark/mixedup')
 library(mixedup)
+library(tidyr)
+library(dplyr)
 
 ############ PROCESSING ###########
 
@@ -39,7 +41,7 @@ summary(df)
 nc_cores <- availableCores()
 
 # Fit a multinomial model using brms
-fit1_multinomial_model = brm(
+fit_multinomial_model = brm(
   # Note: the brms package does a categorical model, but by adding
   # the random effect (1 | x) we are making it multinomial.
   structure ~ distance + (1 | question) + (1 | participant), 
@@ -47,8 +49,8 @@ fit1_multinomial_model = brm(
   family = categorical(link=logit, refcat = NA),
   # Number of available cores
   cores = nc_cores,
-  iter = 2000,
-  warmup = 1000,
+  iter = 2000, #standard: 2000. Reduce for faster processing.
+  warmup = 1000, #standard: 1000. Reduce for faster processing.
   # get all parameters and parameters classes to define priors automatically
   prior = get_prior(structure ~ distance + (1 | question) + (1 | participant), 
                      data = df, family = categorical),
@@ -57,24 +59,53 @@ fit1_multinomial_model = brm(
 )
 
 # Display a summary of the fitted model
-summary(fit1_multinomial_model)
-
-# RESULTS:
-# Intercept overt connective:
-# Intercept juxtaposition:
-# Intercept ellipsis:
+summary(fit_multinomial_model)
 
 # Display random effects for intercepts
-ranef(fit1_multinomial_model)
-
-# Extract information into a nice table
-results <- mixedup::extract_random_effects(fit1_multinomial_model) 
-
-# Visualize conditional effects
-conditional_effects(fit1_multinomial_model, categorical = TRUE, plot = FALSE)
+ranef(fit_multinomial_model)
 
 # Model checking with posterior predictive checks
-pp_check(fit1_multinomial_model)
+pp_check(fit_multinomial_model)
+
+# Extract random effects into a nice table and saves it
+results <- mixedup::extract_random_effects(fit_multinomial_model) 
+ce <- conditional_effects(fit_multinomial_model, categorical = TRUE)
+ce <- as.data.frame(ce[[1]])
+write.csv(ce, file = "results/results_conditional_effects.csv", row.names = FALSE)
+
+# Visualize conditional effects
+conditional_effects(fit_multinomial_model, categorical = TRUE, plot = FALSE)
+
+# To get a table that includes the probability of each category, first we pivot the data
+prob_table <- ce %>%
+  pivot_wider(
+    id_cols = c("distance", "cond__"),
+    names_from = "cats__",
+    values_from = "estimate__"
+  ) %>%
+  select(-cond__) %>%
+  mutate(across(-c(distance), ~ifelse(is.na(.), 0, .)))  # Replace NAs with 0
+# Rename the columns
+colnames(prob_table) <- c("distance", "elliptical", "juxtaposition", "overt_connective")
+# Saves it
+write.csv(prob_table, file = "results/prob_table.csv", row.names = FALSE)
+
+############ RESULTS AFTER LOGISTIC REGRESSION ###########
+
+# ELLIPTICAL:
+summary(lm(distance ~ elliptical, data = prob_table))
+# P-value:
+#Coefficient:
+
+# JUXTAPOSITION:
+summary(lm(distance ~ juxtaposition, data = prob_table))
+# P-value:
+#Coefficient:
+
+# OVERT CONNECTIVE:
+summary(lm(distance ~ overt_connective, data = prob_table))
+# P-value:
+#Coefficient:
 
 ############ REFERENCES ###########
 
